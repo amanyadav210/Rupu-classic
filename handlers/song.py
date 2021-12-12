6cbe5322febea8a76f327789d933c9767e2cb3c2
lukka12 <47463180+lukka12@users.noreply.github.com>
2021-12-12 18:38:18 +0530

    Update song.py

--- handlers/song.py
+++ handlers/song.py
@@ -51,7 +51,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = '📥 ᴜᴘʟᴏᴀᴅᴇᴅ ʙʏ : ɴᴀɴᴅᴜ's sᴇʀᴠᴇʀ 📤'
+        rep = '📥 ᴜᴘʟᴏᴀᴅᴇᴅ ʙʏ : ɴᴀɴᴅᴜ's sᴇʀᴠᴇʀ'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
