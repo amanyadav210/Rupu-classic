121c1a714037b30ae9102e286355e9a3cbc1476e
lukka12 <47463180+lukka12@users.noreply.github.com>
2021-12-12 20:17:40 +0530

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -308,7 +308,6 @@ async def play(_, message: Message):
 
         await message.reply_photo(
             photo="final.png",
-            reply_markup=keyboard,
             caption=f"☑️ **ɴᴀᴍᴇ:** [{title[:80]}]({url})\n⏱ **ᴅᴜʀᴀᴛɪᴏɴ:** `{duration}`\n💡 **ꜱᴛᴀᴛᴜꜱ:** `ᴘʟᴀʏɪɴɢ`\n"
             + f"🎧 **ᴢᴀɪᴅ ᴜꜱᴇʀ ʙʏ:** {message.from_user.mention}",
             reply_markup=keyboard,
