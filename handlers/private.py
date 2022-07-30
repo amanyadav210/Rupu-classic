4cc351613bb32948a0357b8c3bedf5123b7bbb07
amanyadav210 <85675173+amanyadav210@users.noreply.github.com>
2022-07-30 14:54:26 +0530

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -12,23 +12,23 @@ async def start(_, message: Message):
     await message.reply_text(
         f"""**Hᴇʏ ɪᴛs {bn}** \n
 **I ᴀᴍ ʟᴀᴢʏ Aʙᴏᴜᴛ ᴛʏᴘɪɴɢ sᴏᴍᴇᴛʜɪɴɢ ɴᴇᴡ..ƳЄ ЄƘ ƁƠƬ ӇƛƖ ƳЄ ƓƛƝƛ ƁƛƝƛƝЄ ƘЄ ԼƖƳЄ ƁƛƝƛ ӇƛƖ.😈❣️
-Dᴇᴠᴇʟᴏᴘᴇᴅ Bʏ : [ƛMƛƝ](https://t.me/A_4_AMAN_0fficial)**.
+Dᴇᴠᴇʟᴏᴘᴇᴅ Bʏ : [ƛMƛƝ](https://t.me/A_4_AMAN_yadav_0fficial)**.
         """,
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "💞 ᴏᴡɴᴇʀ ", url="https://t.me/A_4_AMAN_0fficial")
+                        "💞 ᴏᴡɴᴇʀ ", url="https://t.me/A_4_AMAN_yadav_0fficial")
                   ],[
                     InlineKeyboardButton(
-                        "🔥ƘƠƖ ƊƖƘƘƛƬ ӇƠ ƊM ƘƦ", url="https://t.me/A_4_AMAN_0fficial"
+                        "🔥ƘƠƖ ƊƖƘƘƛƬ ӇƠ ƊM ƘƦ", url="https://t.me/A_4_AMAN_yadav_0fficial"
                     ),
                     InlineKeyboardButton(
                         "🐬 Cᴏᴍᴍᴀɴᴅ", url="https://telegra.ph/%F0%9D%90%92%F0%9D%9F%92%F0%9D%90%92%F0%9D%90%87%F0%9D%90%88%F0%9D%90%95-%F0%9D%90%97%F0%9D%90%83--%F0%9D%99%B8-%F0%9D%90%80%F0%9D%94%AA-E-%F0%9D%97%A0%F0%9D%94%AC%E2%84%95%F0%93%82%B8-10-16"
                     )
                 ],[ 
                     InlineKeyboardButton(
-                        "ƁӇƠƧƊƖƘЄ ӇƲMƛƦƛ ƁƠƬ ƛƊƊ ƘƦԼƠ 🤭", url=f"https://t.me/http://t.me/AMAN_AK_MUSIC_bot?startgroup=true"
+                        "ƁӇƠƧƊƖƘЄ ӇƲMƛƦƛ ƁƠƬ ƛƊƊ ƘƦԼƠ 🤭", url=f"https://t.me/http://t.me/ASFIGHTERS_Bot?startgroup=true"
                     )]4
             ]
         ),
@@ -42,7 +42,7 @@ async def gstart(_, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        "😎 ᴜᴘᴅᴀᴛᴇs", url="https://t.me/A_4_AMAN_0fficial")
+                        "😎 ᴜᴘᴅᴀᴛᴇs", url="https://t.me/A_4_AMAN_yadav_0fficial")
                 ]
             ]
         )
